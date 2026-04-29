c06dda416d9797c4198b18bc1ca122a49c5f0fb7
Dev-Mjarir08 <08mjarir@gmail.com>
2026-04-29 18:45:01 +0530

    day-2-arry-&-methods

---
+++ day-2-array and methods/index.js
@@ -0,0 +1,80 @@
+let arr = [1, 2];
+
+//push
+arr.push(3);
+console.log(arr);
+
+
+//pop
+arr.pop()
+console.log(arr);; 
+
+
+let arr1 = [1,2,3];
+//shift
+arr1.shift(); 
+console.log(arr1);
+
+
+
+//unshift
+arr1.unshift(0); 
+console.log(arr1);
+
+
+let arr2 = [1,2,3,4];
+//splice
+arr2.splice(1,2);
+console.log(arr2);
+
+//slice
+
+let arr3 = [1,2,3,4];
+let newArr = arr3.slice(1,3);
+
+console.log(newArr); 
+
+//concate
+
+let a = [1,2];
+let b = [3,4];
+
+let c = a.concat(b);
+
+//toString
+console.log(c.toString());
+
+//join
+console.log(c.join('-'));
+
+//map
+let arr4 = [1,2,3];
+
+let result = arr4.map(x => x * 2);
+console.log(result);
+
+//filter
+
+let arr5 = [1,2,3,4];
+
+let result2 = arr5.filter(x=> x > 2);
+console.log(result2);
+
+//forEach
+arr.forEach(x => console.log(x));
+
+//find
+
+ let arr6 = [1,2,3,4];
+
+arr6.find(x => x >= 1);
+console.log(arr6.find(x => x > 3));
+
+//includes
+console.log(arr6.includes(4));
+
+
+//last index
+
+console.log(arr6.lastIndexOf(4));
+
