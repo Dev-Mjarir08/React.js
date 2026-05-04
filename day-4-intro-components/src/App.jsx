9e51b400ed9ff3752716b6d76f0d00b72374b73e
Dev-Mjarir08 <08mjarir@gmail.com>
2026-05-04 12:25:26 +0530

    day-4-intro-components

---
+++ day-4-intro-components/src/App.jsx
@@ -0,0 +1,9 @@
+import Home from "./components/Home/home"
+const App=() => {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+export default App
---
+++ day-4-intro-components/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import './Home.css'
+
+const Home=()=>{
+    return (
+        <>
+        <h1 className='heading'>Hello React Developers</h1>
+        </>
+    )
+}
+export default Home
