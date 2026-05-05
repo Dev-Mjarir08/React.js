2c7398b9c155aa5510f6718959d2862e971dcda1
Dev-Mjarir08 <08mjarir@gmail.com>
2026-05-05 12:00:00 +0530

    day-5-intro-components

--- day-5-intro-components/src/components/Home/Home.jsx
+++ day-5-intro-components/src/components/Home/Home.jsx
@@ -1,3 +1,15 @@
-function greet(name) {
-
+import React from 'react'
+import './Home.css'
+const Home = () => {
+    return (
+        <>
+            <div className='intro'>
+                <h2>Name:Jarir Multani</h2>
+                <p>I'm a React Developer</p>
+                <p>Nice to Meet You..!</p>
+            </div>
+        </>
+    )
 }
+
+export default Home
