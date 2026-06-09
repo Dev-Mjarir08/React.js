6fc3f428d714bafc773450fd58a65d130ff234d9
Dev-Mjarir08 <08mjarir@gmail.com>
2026-06-09 13:09:06 +0530

    day-18-routing-dom

---
+++ day-18-routing-dom/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Header from './components/Header'
+import About from './pages/About'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
